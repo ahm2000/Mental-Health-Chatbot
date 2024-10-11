48999fce8b59bde2e2f0c0420a55b932e9e5ebec
ahm2000 <107981788+ahm2000@users.noreply.github.com>
2024-10-11 11:11:48 +0500

    Add files via upload

---
+++ run.py
@@ -0,0 +1,11 @@
+from ChatbotWebsite import create_app, db
+
+# Create the app
+app = create_app()
+
+with app.app_context():
+    db.create_all() 
+
+# Run the app
+if __name__ == '__main__':
+    app.run()
